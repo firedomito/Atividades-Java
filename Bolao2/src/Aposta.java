5b4e431bd2967f8e345313fec94e9f84d9390ad0
firedomito <guilhermesilveira.2015@hotmail.com>
2021-08-03 18:19:19 -0300

    Create Aposta.java

---
+++ Bolao2/src/Aposta.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Aposta {
+
+    ArrayList<Integer> numeros = new ArrayList<>();
+    Jogador organizador;
+    ArrayList<Jogador> jog = new ArrayList<>();
+
+
+
+
+
+
+
+
+}
