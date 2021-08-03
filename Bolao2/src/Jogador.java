9749e7c18bed796ae56c2281bdc51022d694148e
firedomito <guilhermesilveira.2015@hotmail.com>
2021-08-03 17:08:34 -0300

    Trabalho 2 POO

---
+++ Bolao2/src/Jogador.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Jogador extends Pessoa{
+
+    private String pix;
+
+    Jogador(){
+        super();
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite seu PIX: ");
+        this.setPix(s.nextLine());
+    }
+
+    Jogador(String nome, String cpf, String pix){
+        super(nome, cpf);
+        this.setPix(pix);
+    }
+
+    public void listarDados(){
+        super.listarDados();
+        System.out.println("Pix: " + this.getPix());
+    }
+
+    public String getPix() {
+        return pix;
+    }
+
+    public void setPix(String pix) {
+        this.pix = pix;
+    }
+}
